aa8f64033e2107eea12ab3dded1997d274e95e8e
PavelP78 <166494216+PavelP78@users.noreply.github.com>
2024-05-19 20:55:26 +0300

    Delete tests/test_url.py

--- tests/test_url.py
+++
@@ -1,9 +0,0 @@
-from locators.locators import ScooterLocators
-from data import ScooterUrl as Url
-
-
-class TestScooterYandexLogo:
-
-
-    def test_main_page_logo(self, driver):
-        assert driver.current_url == f'{Url.URL_MAIN}', "Url is wrong"
