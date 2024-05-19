5112881e4967942f37c2a1de641c627a4da68f3c
PavelP78 <166494216+PavelP78@users.noreply.github.com>
2024-05-19 20:53:30 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,40 +1,19 @@
 import pytest
 from selenium import webdriver
-from config import ScooterResolution as Resolution
 from data import ScooterUrl as Url
 
-def browser_setting():
-     firefox_option = webdriver.FirefoxOptions()
-     firefox_option.add_argument(f'--window-size = {Resolution.RESOLUTION[0]}, {Resolution.RESOLUTION[1]}')
-     return firefox_option
+
 
 @pytest.fixture
 def driver():
-     firefox = webdriver.Firefox(options=browser_setting())
-     firefox.get(Url.URL_MAIN)
-
-     yield firefox
-
-     firefox.quit()
+     driver = webdriver.Firefox()
+     driver.maximize_window()
+     driver.get(Url.URL_MAIN)
 
 
-# ======
-# import pytest
-# from selenium import webdriver
-# from config import ScooterResolution as Resolution
-# from data import ScooterUrl as Url
+     driver.delete_all_cookies()
 
-# def browser_setting():
-#      chrome_option = webdriver.ChromeOptions()
-#      chrome_option.add_argument(f'--window-size = {Resolution.RESOLUTION[0]}, {Resolution.RESOLUTION[1]}')
-#      return chrome_option
-#
-# @pytest.fixture
-# def driver():
-#      chrome = webdriver.Chrome(options=browser_setting())
-#      chrome.get(Url.URL_MAIN)
-#
-#      yield chrome
-#
-#      chrome.quit()
+     yield driver
+
+     driver.quit()
 
