2a4d41af993d9a8edb713419db23f9d8b09dc33d
PavelP78 <166494216+PavelP78@users.noreply.github.com>
2024-05-19 20:52:54 +0300

    Update data.py

--- data.py
+++ data.py
@@ -1,21 +1,23 @@
 
-
-class StellarBurgersUrl:
-
-    URL_MAIN = 'https://stellarburgers.nomoreparties.site/'
-    URL_LOGIN = f'{URL_MAIN}login'
-    URL_ACCOUNT = f'{URL_MAIN}account'
-    URL_ACCOUNT_PROFILE = f'{URL_MAIN}account/profile'
-
-
-class StellarBurgersLogin:
-
-    MY_LOGIN = 'pavel_pliasunov_8_888@yandex.ru'
-    MY_PASSWORD = '12345678'
-
 class ScooterUrl:
 
     URL_DZEN = 'https://dzen.ru/'
     URL_MAIN = 'https://qa-scooter.praktikum-services.ru/'
 
+    URL_LOGIN = f'{URL_MAIN}login'
+    URL_ACCOUNT = f'{URL_MAIN}account'
+    URL_ACCOUNT_PROFILE = f'{URL_MAIN}account/profile'
+
+
+class ScooterLoginUp:
+
+    LOGIN = 'Виктор'
+    SURNAME = "Иванов"
+    ADRESS = "Мира 12"
+    PHONE = "89112000000"
+
+    LOGIN_2 = 'Алексей'
+    SURNAME_2 = "Смирнов"
+    ADRESS_2 = "Труда 9А"
+    PHONE_2 = "89212999999"
 
